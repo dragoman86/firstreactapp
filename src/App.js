d88c1a8f875025a806ba1d1256388752aca8744b
dragoman86 <walid.benna@gmail.com>
2022-11-18 15:24:13 +0100

    add app.js 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,138 @@
 import logo from './logo.svg';
 import './App.css';
+import { Card, Nav, Placeholder, Alert, Button , Form, Row, Col, Container, Badge} from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
+      <div>
+       <Nav
+      activeKey="/home"
+      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+    >
+      <Nav.Item>
+        <Nav.Link href="/home">Home</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-1">Library</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-2">Data</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="disabled" disabled>
+          Extra
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+    </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     
+      <container>
+      <h1><Badge bg="secondary">Sign IN!</Badge></h1>
+      <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder ="Enter email" animation="glow" />
+            <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+            </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+
+      <Button variant="primary" type="reset">
+        Reset
+      </Button>
+
+    </Form> 
+    </container>
       </header>
+      <body>
+        <container >
+        <h1>SUBSCRIPTION</h1>
+        <Form>
+          <Row>
+            <Col>
+              <Form.Control placeholder="First name" />
+            </Col>
+            <Col>
+              <Form.Control placeholder="Last name" />
+            </Col>
+          </Row>
+    </Form>
+    <Form>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+      </Row>
+
+      <Form.Group className="mb-3" controlId="formGridAddress1">
+        <Form.Label>Address</Form.Label>
+        <Form.Control placeholder="1234 Main St" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formGridAddress2">
+        <Form.Label>Address 2</Form.Label>
+        <Form.Control placeholder="Apartment, studio, or floor" />
+      </Form.Group>
+
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridCity">
+          <Form.Label>City</Form.Label>
+          <Form.Control />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Label>State</Form.Label>
+          <Form.Select defaultValue="Choose...">
+            <option>Choose...</option>
+            <option>...</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridZip">
+          <Form.Label>Zip</Form.Label>
+          <Form.Control />
+        </Form.Group>
+      </Row>
+
+      <Form.Group className="mb-3" id="formGridCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+    <Row>
+      <Col>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+      </Col>
+      <Col>
+      <Button variant="primary" type="reset">
+        Reset
+      </Button>
+      </Col>
+      </Row>
+    </Form>
+    </container>
+      </body>
+      <footer>First APP Benna WALID - GoMyCode - Yasmine Web II</footer>
     </div>
   );
 }
